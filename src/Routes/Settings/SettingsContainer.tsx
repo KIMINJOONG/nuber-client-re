e8b2a9a6558da348d1c12770b791b5c9f0251a9b
KIMINJOONG <ijboym216@gmail.com>
2020-04-06 17:42:15 +0900

    2.42 Settings Screen part Two

--- src/Routes/Settings/SettingsContainer.tsx
+++ src/Routes/Settings/SettingsContainer.tsx
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
 import SettingsPresenter from "./SettingsPresenter";
 import { Mutation, Query } from "react-apollo";
-import { USER_PROFILE } from "../../sharedQueries";
-import { userProfile } from "../../types/api";
+import { USER_PROFILE, GET_PLACES } from "../../sharedQueries";
+import { userProfile, getPlaces } from "../../types/api";
 import { LOG_USER_OUT } from "../../sharedQueries.local";
 
 class MiniProfileQuery extends Query<userProfile> {}
+
+class PlacesQuery extends Query<getPlaces> {}
+
 class SettingsContainer extends Component {
     render() {
         return (
             <Mutation mutation={LOG_USER_OUT}>
                 {(logUserOut) => (
                     <MiniProfileQuery query={USER_PROFILE}>
-                        {({ data, loading: userDataLoading }) => (
-                            <SettingsPresenter
-                                userDataLoading={userDataLoading}
-                                userData={data}
-                                logUserOut={logUserOut}
-                            />
+                        {({ data: userData, loading: userDataLoading }) => (
+                            <PlacesQuery query={GET_PLACES}>
+                                {({
+                                    data: placesData,
+                                    loading: placesLoading,
+                                }) => (
+                                    <SettingsPresenter
+                                        userDataLoading={userDataLoading}
+                                        placesLoading={placesLoading}
+                                        userData={userData}
+                                        placesData={placesData}
+                                        logUserOut={logUserOut}
+                                    />
+                                )}
+                            </PlacesQuery>
                         )}
                     </MiniProfileQuery>
                 )}
